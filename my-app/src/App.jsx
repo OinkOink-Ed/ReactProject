d3d68d8138c38059fa18d8d067476ffbbb3fb2d8
OinkOink <oinkoinkpw@gmail.com>
2022-10-27 16:14:23 +0300

    Настроеные стили и выполнено форматирование файлов

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -5,19 +5,19 @@ import "./App.scss";
 function App() {
   return (
     <>
-      <Navbar      fixed     = "top" expand = "lg">
-      <Container   className = "d-flex justify-content-center">
-      <Navbar.Text className = "text-center">ГБУЗ "ГП №1" г.Сочи Запись на обучение</Navbar.Text>
+      <Navbar fixed="top" expand="lg">
+        <Container className="d-flex justify-content-center">
+          <Navbar.Text className="text-center">ГБУЗ "ГП №1" г.Сочи Запись на обучение</Navbar.Text>
         </Container>
       </Navbar>
-      <ChoiseOfStep/>
-      <Navbar fixed = "bottom" expand = "lg">
+      <ChoiseOfStep />
+      <Navbar fixed="bottom" expand="lg">
         <Container className="d-flex justify-content-center">
           <Navbar.Text className="text-center">По вопросам предложений и улучшений обращаться по номеру: 8-952-836-06-42</Navbar.Text>
         </Container>
       </Navbar>
     </>
   );
-}
+};
 
 export default App;
--- my-app/src/ChoiseOfStep.jsx
+++ my-app/src/ChoiseOfStep.jsx
@@ -2,11 +2,11 @@ import "./ChoiseOfStep.scss";
 import { Button } from "react-bootstrap";
 
 function ChoiseOfStep() {
-  return(
-    <div    id        = "choiseOfStep" className = "d-flex justify-content-center align-items-center min-vh-100">
-    <div    className = "d-flex justify-content-center flex-wrap">
-    <Button variant   = "success" size           = "sm">Войти</Button>
-    <Button variant   = "primary" size           = "sm">Записаться на прием</Button>
+  return (
+    <div id="choiseOfStep" className="d-flex justify-content-center align-items-center min-vh-100">
+      <div className="d-flex justify-content-center flex-wrap">
+        <Button variant="success" size="sm">Войти</Button>
+        <Button variant="primary" size="sm">Записаться на прием</Button>
       </div>
     </div>
   );
