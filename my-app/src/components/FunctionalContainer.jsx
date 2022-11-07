5bcc9cafc90c4eb49ecdad42f2fff1ae6cc1f239
OinkOink <oinkoinkpw@gmail.com>
2022-11-07 15:49:12 +0300

    незнаительные корректировки

--- my-app/src/components/FunctionalContainer.jsx
+++ my-app/src/components/FunctionalContainer.jsx
@@ -1,12 +1,20 @@
 import Container from "./UI/container/Container";
 import EnterOrRecord from "./EnterOrRecord";
+import { useState } from "react";
 
 function FunctionalContainer() {
-    return (
-        <Container className="choise_of_container">
-            <EnterOrRecord className="enter_or_record"></EnterOrRecord>
-        </Container>
-    );
+    const [functionalBody, SetfunctionalBody] = useState({
+        entriesForm: false,
+        recordForm: false,
+    });
+
+    if (!(functionalBody.entriesForm || functionalBody.recordForm)) {
+        return (
+            <Container className="choise_of_container">
+                <EnterOrRecord className="enter_or_record"></EnterOrRecord>
+            </Container>
+        );
+    };
 };
 
 export default FunctionalContainer;
