9892d5337121dfa88cafb378228bec31bc2c692d
OinkOink-Ed <oinkoinkpw@gmail.com>
2022-11-07 21:20:50 +0300

    Немного изменил стили. Создал переменные цвета

--- my-app/src/components/EnterOrRecord.jsx
+++ my-app/src/components/EnterOrRecord.jsx
@@ -5,8 +5,8 @@ function EnterOrRecord({ onChange, ...props }) {
     return (
         <Container {...props}>
             <Container className="groups_btn_container m-2 mt-4 mb-4 flex-wrap justify-content-center d-flex">
-                <Button className="btn p-1 m-2 justify-content-center" onClick={onChange}>Войти как администратор</Button>
-                <Button className="btn p-1 m-2 justify-content-center">Записаться на обучение</Button>
+                <Button className="btn bg-green br-green p-1 m-2 justify-content-center" onClick={onChange}>Войти как администратор</Button>
+                <Button className="btn bg-teal br-teal p-1 m-2 justify-content-center">Записаться на обучение</Button>
             </Container>
         </Container>
     )
