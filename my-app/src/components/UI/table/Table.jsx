874ed0357547202a86d2d9310a03c0b090fd5fae
OinkOink <oinkoinkpw@gmail.com>
2022-11-29 19:33:37 +0300

    Мой первый запрос к Серверу

---
+++ my-app/src/components/UI/table/Table.jsx
@@ -0,0 +1,15 @@
+function Table(props) {
+
+    let content = props.contents;
+    console.log(content);
+
+    return (
+        <table>
+            <caption></caption>
+            <thead></thead>
+            <tbody></tbody>
+        </table>
+    );
+};
+
+export default Table;
--- my-app/src/pages/Admin.jsx
+++ my-app/src/pages/Admin.jsx
@@ -1,21 +1,35 @@
 import { useState } from "react";
+import Table from "../components/UI/table/Table";
 
 function Admin() {
 
-    let [data, Setdata] = useState(null);
+    let [content, Setdata] = useState();
 
     async function onHandlerClick() {
-        await fetch("http://oinkoink").then(function (response) {
-            response.text().then(function (text) {
-                Setdata(data = text);
-            });
+        let response = await fetch("http://oinkoink", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                "type": "opt_1",
+            }),
         });
+
+        if (response.ok) {
+            let data = await response.json();
+            Setdata(data);
+        } else {
+            alert("HTTP ошибка сервера: " + response.status);
+        };
     };
 
     return (
         <div>
             <button onClick={onHandlerClick}>Получить данные из БД</button>
-            <div>{data}</div>
+            <div>
+                <Table contents={content}></Table>
+            </div>
         </div>
     );
 };
