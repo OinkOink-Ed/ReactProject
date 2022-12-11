3bbbf946bcf3de58f3de172eeed84f0b818a768d
OinkOink <oinkoinkpw@gmail.com>
2022-12-11 16:44:45 +0300

    Внес изменения в ещё один компонент

--- my-app/src/components/EntriesForm.jsx
+++ my-app/src/components/EntriesForm.jsx
@@ -1,8 +1,7 @@
 import Container from "./UI/containers/Container";
 import Form from "./UI/form/Form";
-import Input from "./UI/input/Input";
 import Button from "./UI/button/button";
-import Label from "./UI/label/Label";
+import ElFormContainer from "../components/UI/containers/ElFormContainer";
 import { useNavigate } from 'react-router-dom';
 
 function EntriesForm({ onBack, ...props }) {
@@ -15,14 +14,8 @@ function EntriesForm({ onBack, ...props }) {
 
     return (
         <Form {...props}>
-            <Container className="groups-elemnts-form">
-                <Label className="label-input">Введите Логин</Label>
-                <Input className="input-form" placeholder="Логин" type="text"></Input>
-            </Container>
-            <Container className="groups-elemnts-form">
-                <Label className="label-input">Введите пароль</Label>
-                <Input className="input-form" placeholder="Пароль" type="current-password"></Input>
-            </Container>
+            <ElFormContainer textLabel="Введите Логин" holderInput="Логин" typeInput="text"></ElFormContainer>
+            <ElFormContainer textLabel="Введите Пароль" holderInput="Пароль" typeInput="current-password"></ElFormContainer>
             <Container className="groups-btn-form">
                 <Button className="btn-back btn"
                     onClick={onBack}>
